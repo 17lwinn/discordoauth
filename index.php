4b95bbf6c893875e0de5fe5b6108b9b058f1fa59
Glitch (lamp-poc) <none>
2020-05-16 22:55:28 +0000

    🍖🐄 Checkpoint ./index.php:9099011/299 ./.htaccess:9099011/89 ./login.php:9099011/3891

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
-<html>
-  <head>
-    <title>LAMP Proof of Concept</title>
-  </head>
-  <body>
-    <?php
-    $number = 5;
-    echo "The number of the day is $number!";
-    ?>
-  </body>
-</html>
+<?php
+header("Location: /login");
+die();
+?>
